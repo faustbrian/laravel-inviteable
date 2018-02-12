c2fa3fa6adeb2c71d5c4fe705ec86b320edbfbfe
Brian Faust <faustbrian@users.noreply.github.com>
2018-02-12 08:45:01 +0200

    Apply fixes from StyleCI (#11)
    [ci skip] [skip ci]

--- src/Models/Invite.php
+++ src/Models/Invite.php
@@ -55,7 +55,7 @@ class Invite extends Model
      */
     public function claimed(): bool
     {
-        return ! empty($this->claimed_at);
+        return !empty($this->claimed_at);
     }
 
     /**
